60a30b33262914635f2966fa7833308a9527adbc
Rin <64449865+lee-haell@users.noreply.github.com>
2023-02-06 23:16:36 +0900

    edit-scroll script 수정, 위젯 포폴 링크 수정

--- js/app.js
+++ js/app.js
@@ -7,41 +7,57 @@ $(function(){
     let book = $('.book');
 
     $(window).scroll(function(){
+        let browserHeight = window.innerHeight;
         const scrollValue = $(document).scrollTop();
 
-        if(scrollValue >= 300){
+        // if(scrollValue >= 300){
+        //     coverStr.addClass('openActive1');
+        //     book.addClass('openActive1');
+        // } else {
+        //     coverStr.removeClass('openActive1');
+        //     book.removeClass('openActive1');
+        // }
+
+        // if(scrollValue >= 500){
+        //     coverStr.addClass('openActive2');
+        //     book.addClass('openActive2');
+        // } else {
+        //     coverStr.removeClass('openActive2');
+        //     book.removeClass('openActive2');
+        // }
+
+        // if(scrollValue >= 700){
+        //     coverStr.addClass('openActive3');
+        //     book.addClass('openActive3');
+        // }
+
+        // if(scrollValue >= 800){
+        //     coverWrap.fadeOut(500);
+        //     coverWrap.siblings('.wrap').removeClass('hide');
+        // }
+
+
+        if(scrollValue >= browserHeight/10){
             coverStr.addClass('openActive1');
             book.addClass('openActive1');
-        } else {
-            coverStr.removeClass('openActive1');
-            book.removeClass('openActive1');
-        }
-
-        if(scrollValue >= 500){
+        } 
+        if(scrollValue >= browserHeight/6){
             coverStr.addClass('openActive2');
             book.addClass('openActive2');
-        } else {
-            coverStr.removeClass('openActive2');
-            book.removeClass('openActive2');
         }
-
-        if(scrollValue >= 700){
+        if(scrollValue >= browserHeight/4){
             coverStr.addClass('openActive3');
             book.addClass('openActive3');
         }
-
-        if(scrollValue >= 800){
+        if(scrollValue >= browserHeight/2){
             coverWrap.fadeOut(500);
             coverWrap.siblings('.wrap').removeClass('hide');
         }
 
-        
 
 
 
-
-
-
+        console.log(browserHeight);
         console.log(scrollValue);
     });
 
