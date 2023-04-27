9599e7beb7aac8405f00d0b53be66c32920538b6
Rin <64449865+lee-haell@users.noreply.github.com>
2023-04-27 18:06:04 +0900

    add-포폴 레이아웃 변경 및 locomotive-scroll 적용중

---
+++ js/again.js
@@ -0,0 +1,24 @@
+$(function(){
+
+  /* 메뉴영역 click */
+  const menu = $('.menu');
+
+  menu.click(function(){
+    menu.removeClass('active');
+    $(this).addClass('active');
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 $(function(){
     // scroll animation
-    //
-    //
     let coverWrap = $('.bookCoverWrap');
     let coverStr = $('.coverStrInner');
     let book = $('.book');
@@ -9,33 +7,6 @@ $(function(){
     $(window).scroll(function(){
         let browserHeight = window.innerHeight;
         const scrollValue = $(document).scrollTop();
-
-        // if(scrollValue >= 300){
-        //     coverStr.addClass('openActive1');
-        //     book.addClass('openActive1');
-        // } else {
-        //     coverStr.removeClass('openActive1');
-        //     book.removeClass('openActive1');
-        // }
-
-        // if(scrollValue >= 500){
-        //     coverStr.addClass('openActive2');
-        //     book.addClass('openActive2');
-        // } else {
-        //     coverStr.removeClass('openActive2');
-        //     book.removeClass('openActive2');
-        // }
-
-        // if(scrollValue >= 700){
-        //     coverStr.addClass('openActive3');
-        //     book.addClass('openActive3');
-        // }
-
-        // if(scrollValue >= 800){
-        //     coverWrap.fadeOut(500);
-        //     coverWrap.siblings('.wrap').removeClass('hide');
-        // }
-
 
         if(scrollValue >= browserHeight/10){
             coverStr.addClass('openActive1');
@@ -53,12 +24,6 @@ $(function(){
             coverWrap.fadeOut(500);
             coverWrap.siblings('.wrap').removeClass('hide');
         }
-
-
-
-
-        console.log(browserHeight);
-        console.log(scrollValue);
     });
 
 });
