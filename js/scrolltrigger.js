ad939140cc055c0bc7e44171ca1aef5e11b6ab79
Rin <64449865+lee-haell@users.noreply.github.com>
2023-05-06 00:30:53 +0900

    edit-메인페이지 수정중

--- js/scrolltrigger.js
+++ js/scrolltrigger.js
@@ -1,37 +1,43 @@
 // ScrollTrigger 초기화
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
+
+const color = "opacity-0";
 
 // locomotive-scroll 초기화
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector(".inner"),
+const scroller = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
   smooth: true,
+  tablet: { smooth: true },
+  smartphone: { smooth: true }
 });
+
+
 
 // ScrollTrigger와 locomotive-scroll 연결
-ScrollTrigger.scrollerProxy(".inner", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  },
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  pinType: document.querySelector(".inner").style.transform ? "transform" : "fixed"
-});
+// ScrollTrigger.scrollerProxy(".inner", {
+//   scrollTop(value) {
+//     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+//   },
+//   getBoundingClientRect() {
+//     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+//   },
+//   pinType: document.querySelector(".inner").style.transform ? "transform" : "fixed"
+// });
 
-locoScroll.on("scroll", ScrollTrigger.update);
+// locoScroll.on("scroll", ScrollTrigger.update);
 
-// ScrollTrigger 트리거
-ScrollTrigger.create({
-  trigger: ".intro:nth-child(3)",
-  scroller: ".inner",
-  start: "top 0",
-  end: "bottom 0",
-  onToggle: self => {
-    if (self.isActive) {
-      gsap.to(".circle", {scale: 5});
-    } else {
-      gsap.to(".circle", {scale: 1});
-    }
-  },
-  markers: true // 개발 시에만 사용하는 옵션
-});
+// // ScrollTrigger 트리거
+// ScrollTrigger.create({
+//   trigger: ".intro:nth-child(3)",
+//   scroller: ".inner",
+//   start: "top 0",
+//   end: "bottom 0",
+//   onToggle: self => {
+//     if (self.isActive) {
+//       gsap.to(".circle", {scale: 5});
+//     } else {
+//       gsap.to(".circle", {scale: 1});
+//     }
+//   },
+//   markers: true // 개발 시에만 사용하는 옵션
+// });
