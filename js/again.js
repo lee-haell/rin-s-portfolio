ff42716a41a1ab772d8f2dfdf2229b29ee0a3c1e
Rin <64449865+lee-haell@users.noreply.github.com>
2023-05-07 15:11:29 +0900

    delete-불필요한 코드 삭제

--- js/again.js
+++ js/again.js
@@ -8,26 +8,4 @@ $(function(){
     $(this).addClass('active');
   });
 
-
-  /* scroll event */
-  // $(window).scroll(function(){
-  //   const circle = $('.circle');
-  //   let browserHeight = window.innerHeight;
-  //   const scrollValue = $(document).scrollTop();
-
-  //   console.log(scrollValue);
-
-  //   if(scrollValue >= browserHeight/2){
-  //     circle.addClass('scroll');
-  //   }
-
-  // });
-
-
-
-
-
-
-
-
 });
